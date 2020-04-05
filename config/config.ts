1c777f0b6a672c38af3d814e8bc33f42ede9cf5c
Andrew Burgess <andrew@burgess.io>
2020-04-04 23:21:37 -0400

    change default locale

--- config/config.ts
+++ config/config.ts
@@ -19,7 +19,7 @@ export default defineConfig({
   },
   locale: {
     // default zh-CN
-    default: 'zh-CN',
+    default: 'en-CA',
     // default true, when it is true, will use `navigator.language` overwrite default
     antd: true,
     baseNavigator: true,
