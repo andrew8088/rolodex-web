a23c557654be0069f6ff90575375557068e06268
Andrew Burgess <andrew@burgess.io>
2020-04-05 15:40:58 -0400

    initial cra app

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Button from 'antd/es/button';
+
+function App() {
+  return (
+    <div className="App">
+        <Button type="primary">Button</Button>
+    </div>
+  );
+}
+
+export default App;
