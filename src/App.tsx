6b79e740c56a7e172a8282fbc10f38f9fc9df4f4
Andrew Burgess <andrew@burgess.io>
2020-04-05 21:08:23 -0400

    hello world app

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
-import Button from 'antd/es/button';
+import Statistic from 'antd/es/statistic';
+import Spin from 'antd/es/spin';
+
+const API = process.env.REACT_APP_API_URL;
 
 function App() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [uptime, setUptime] = useState(-1);
+
+  useEffect(() => {
+    fetch(`${API}/status`)
+      .then(res => res.json())
+      .then((res: any) => {
+        setIsLoading(false);
+        setUptime(res.uptime);
+      });
+  }, []);
+
   return (
     <div className="App">
-        <Button type="primary">Button</Button>
+      {isLoading && <Spin size="large" />}
+      {!isLoading && <Statistic title="Uptime" value={uptime}></Statistic> }
     </div>
   );
 }
